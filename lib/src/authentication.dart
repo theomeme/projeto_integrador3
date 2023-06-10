f7b1f85504cf3c6fa55b098b8c74727e3c0b6034
Justamand <justamandd@gmail.com>
2023-06-10 15:43:09 -0300

    :feat: getting firebase auth and google cloud messaging

---
+++ lib/src/authentication.dart
@@ -0,0 +1,40 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class Authentication{
+  Future<void> getAuth() async {
+    final SharedPreferences preferences = await SharedPreferences.getInstance();
+    try {
+      final userCredential = await FirebaseAuth.instance.signInAnonymously();
+      final fcmToken = await FirebaseMessaging.instance.getToken();
+      await preferences.setString('rescuerUid', userCredential.user!.uid);
+      await preferences.setString('fcmToken', fcmToken!);
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case "operation-not-allowed":
+          print("Anonymous auth hasn't been enabled for this project.");
+          break;
+        default:
+          print("Unknown error.");
+      }
+    }
+  }
+
+  Future<List<String>> findUserInfo() async {
+    try {
+      final SharedPreferences preferences = await SharedPreferences.getInstance();
+
+      final rescuerUid = preferences.getString('rescuerUid');
+      final fcmToken = preferences.getString('fcmToken');
+
+      final List<String> userInfo = [rescuerUid!, fcmToken!];
+
+      return userInfo;
+
+    } catch (e) {
+      await getAuth();
+      return await findUserInfo();
+    }
+  }
+}
